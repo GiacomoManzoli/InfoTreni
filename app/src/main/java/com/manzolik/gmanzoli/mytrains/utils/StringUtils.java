7a6e4e4f1310bff742e4e342d567c93d0d3c49f3
Giacomo <giacomo.manzoli@gmail.com>
2016-10-09 22:53:13 +0200

    Creata classe per le funzioni d'utilità

---
+++ app/src/main/java/com/manzolik/gmanzoli/mytrains/utils/StringUtils.java
@@ -0,0 +1,22 @@
+package com.manzolik.gmanzoli.mytrains.utils;
+
+/**
+ * Classe con i metodi d'utilità per lavorare con le stringhe
+ */
+
+public class StringUtils {
+
+
+    public static String capitalizeString(String str){
+        String[] words = str.split(" ");
+        StringBuilder ret = new StringBuilder();
+        for(int i = 0; i < words.length; i++) {
+            ret.append(Character.toUpperCase(words[i].charAt(0)));
+            ret.append(words[i].substring(1).toLowerCase());
+            if(i < words.length - 1) {
+                ret.append(' ');
+            }
+        }
+        return ret.toString().trim();
+    }
+}
